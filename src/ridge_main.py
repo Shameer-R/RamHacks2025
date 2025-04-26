7afc47ae7e3a5f70899fcce06c1568a02cc9b67e
tombour <ridgerouzier@gmail.com>
2025-04-26 15:44:52 -0400

    ridges main

--- src/ridge_main.py
+++ src/ridge_main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import geopy
+import geocoder
+from geopy.geocoders import Nominatim
 import json
 import numpy as np
 
 # Load data files
-
+ 
 
 def load_data():
     # Load Data
@@ -30,21 +33,20 @@ def load_data():
         return None
 
 def main():
-    at_the_crime = [];
+    devices_at_crime = [];
+    geolocator = Nominatim(user_agent="hackathon")
     incidents_json, pings_json, suspects_json, bike_logs_json, snapshots_json = load_data();
-#    print(incidents_json[0]['date'])
- #   print(str.partition(pings_json[0]['timestamp'],'T')[0])
 
     for incident in incidents_json:
         for pings in pings_json:
             if pings['timestamp']>=incident['entry_time'] and pings['timestamp'] <=incident['exit_time']:
-                at_the_crime.append(pings['device_id'])
-    
-    for i in at_the_crime:
-                                    print(i)
-
-
-
-
-
+                location = geolocator.reverse((f"{pings['lat']}" +","+ f"{pings['lon']}"))
+                first_partition = str.partition(location.address,", ")
+                street_number = first_partition[0]
+                street= str.partition(first_partition[2]," ")[0]
+                if street == str.partition(str.partition(incident['address']," ")[2]," ")[0]:
+                        print(street + " "+ street_number)
+                        devices_at_crime.append(pings['device_id'])
+    for i in devices_at_crime:
+        print(i)
 main()
